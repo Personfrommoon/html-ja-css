0356d969afcf460ac0066330ce2a1a7f5dce48eb
Personfrommoon <kristofer.v06@gmail.com>
2024-09-17 17:34:49 +0300

    domainshop ühenduses mysqliga

--- domainshop/server.js
+++ domainshop/server.js
@@ -1,39 +1,65 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-    console.log(req.url);
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-    let domeen = "";
-    let email = "";
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-    let [path, querystring] = req.url.split('?');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'valk',
+    database: 'domeen'
+});
 
-    let query = new URLSearchParams(querystring);
-
-    let data = '';
-    if (path == '/') {
-        data = fs.readFileSync("./vaade1.html", "utf8");
+db.connect((err) => {
+    if (err) {
+        throw err;
     }
+    console.log('Connected to database');
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'vaade1.html'));
+});
 
-    if (path == '/vaade2.html') {
-        data = fs.readFileSync("./vaade2.html", "utf8");
-    }
+app.get('/vaade2.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'vaade2.html')); 
+});
 
-    if (path == '/vaade3.html') {
-        data = fs.readFileSync("./vaade3.html", "utf8");
-    }
+app.get('/vaade3.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'vaade3.html')); 
+});
 
-    if (path == '/vaade4.html') {
-        data = fs.readFileSync("./vaade4.html", "utf8");
-    }
+app.get('/vaade4.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'vaade4.html')); 
+});
 
-    if (path == '/vaade5.html') {
-        data = fs.readFileSync("./vaade5.html", "utf8");
-    } 
+app.get('/vaade5.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'vaade5.html')); 
+});
 
-    if (path == '/vaade7.html') {
-        data = fs.readFileSync("./vaade7.html", "utf8");
-    }
+app.get('/vaade7.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'vaade7.html')); 
+});
+
+app.post('/vaade7.html', (req, res) => {
+    console.log('Received POST request with body:', req.body);
+
+    const { domeen, ending, Name, email, password, username, countryCodeSelect, phoneNumberInput } = req.body;
+
+    const query = 'INSERT INTO domeenid (domeen, ending, name, email, password, username, countryCodeSelect, phoneNumberInput) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+
+    db.query(query, [domeen, ending, Name, email, password, username, countryCodeSelect, phoneNumberInput], (err, result) => {
+        if (err) {
+            console.error('Error saving domain information:', err);
+            res.status(500).send('Error saving domain information.');
+        }
+    });
+});
 
     /* 
      let [path, query] = req.url.split("?");
@@ -45,6 +71,6 @@ http.createServer(function (req, res) {
      }
     */
 
-    res.write(data);
-    res.end();
-}).listen(8080);
+  app.listen(8080, () => {
+    console.log('Server is running on http://localhost:8080')
+});
